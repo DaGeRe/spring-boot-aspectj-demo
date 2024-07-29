a2e8f7571220b7937723a115269186a290ebb904
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-07-29 14:18:04 +0200

    Use project for logging backend example

--- aspect/src/main/java/de/aspectjtest/ExampleAspect.java
+++ aspect/src/main/java/de/aspectjtest/ExampleAspect.java
@@ -4,22 +4,26 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Aspect
 public class ExampleAspect {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ExampleAspect.class);
+
 	@Pointcut("!within(de.aspectjtest..*)")
-	public void notWithinAspect() {
-	}
+	public void notWithinAspect() {}
 
 	@Pointcut("!set(private * *)")
 	public void noSet() {
-		
+
 	}
-	
+
 	@Before("notWithinAspect() && noSet()")
 	public void aroundStuff(final JoinPoint thisJoinPoint)
 			throws Throwable {
 		System.out.println("=== Call1: " + thisJoinPoint.getSignature() + " " + thisJoinPoint.getKind());
+		LOGGER.info("Method was called");
 	}
 }
